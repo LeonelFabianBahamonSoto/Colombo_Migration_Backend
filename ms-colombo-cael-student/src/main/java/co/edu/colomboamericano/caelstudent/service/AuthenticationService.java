38ab7704b1851ceb09c8c662537ddee386cab409
LeonelFabianBahamonSoto <111816323+LeonelFabianBahamonSoto@users.noreply.github.com>
2022-09-28 18:45:27 -0500

    Delete ms-colombo-cael-student directory

--- ms-colombo-cael-student/src/main/java/co/edu/colomboamericano/caelstudent/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package co.edu.colomboamericano.caelstudent.service;
-
-import co.edu.colomboamericano.caelstudent.entity.Student;
-
-public interface AuthenticationService
-{
-	Object signInAndReturnJWT(Student signInRequest);
-}
